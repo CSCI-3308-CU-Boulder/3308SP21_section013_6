d7142d4946bf0fa4faa3c7eb4fd5087b6c748753
SpencyWency <spencerstepanek@gmail.com>
2021-02-16 13:05:01 -0700

    Test directory added

--- Project Code/Python_Backend/main.py
+++ Project Code/Python_Backend/main.py
@@ -4,7 +4,26 @@
 # Created on 2/16/21
 
 
+import numpy as np
 import cv2
+
 
 print("Test!!!")
 
+fabio_path = 'fabio.jpg'
+lenna_path = 'lenna.png'
+
+# Load Fabio in grayscale
+fabio_gray = cv2.imread(fabio_path, 0)
+
+# Load Lenna in grayscale
+lenna_gray = cv2.imread(lenna_path, 0)
+
+cv2.imshow('Fabio',fabio_gray)
+cv2.imshow('Lenna',lenna_gray)
+
+print("fabio_gray:\n.format{}", fabio_gray)
+print("fabio_gray:\n.format{}", fabio_gray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
