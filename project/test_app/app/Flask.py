ec122b3a059fe395af6e94d752eee8c8da7a753c
Spacener <spst7341@colorado.edu>
2021-03-02 13:10:50 -0700

    Flask implementation with docker

--- project/test_app/app/Flask.py
+++ project/test_app/app/Flask.py
@@ -1,5 +1,7 @@
-from flask import render_template
-from app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 
 @app.route("/home")
 def home():
@@ -14,3 +16,7 @@ def login():
 @app.route("/account_reg")
 def account_reg():
     return render_template("account_reg.html")
+
+
+if __name__ == "__main__":
+    app.run()
