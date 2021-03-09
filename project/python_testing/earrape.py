b038c9e475e8bee518c8ff3fe3802887cf2daf0d
Spacener <spst7341@colorado.edu>
2021-03-09 14:12:58 -0700

    Earrape committed, sins not attoned for

--- project/python_testing/earrape.py
+++ project/python_testing/earrape.py
@@ -3,30 +3,28 @@
 # CSCI_3308, Photophonic Web-App Project
 # Created on 3/9/21
 
+from tones import SINE_WAVE, SAWTOOTH_WAVE
+from tones.mixer import Mixer
 
-import pyaudio     #sudo apt-get install python-pyaudio
-import numpy as np
+# Create mixer, set sample rate and amplitude
+mixer = Mixer(44100, 0.5)
 
-p = pyaudio.PyAudio()
+# Create two monophonic tracks that will play simultaneously, and set
+# initial values for note attack, decay and vibrato frequency (these can
+# be changed again at any time, see documentation for tones.Mixer
+mixer.create_track(0, SAWTOOTH_WAVE, vibrato_frequency=7.0, vibrato_variance=30.0, attack=0.01, decay=0.1)
+mixer.create_track(1, SINE_WAVE, attack=0.01, decay=0.1)
 
-volume = 0.06     # range [0.0, 1.0]
-fs = 44100       # sampling rate in Hz, int
-duration = 1.0   # duration of audio in seconds, may be float
-f = 440.0        # sine frequency, Hz, may be float
+# Add a 1-second tone on track 0, slide pitch from c# to f#)
+mixer.add_note(0, note='c#', octave=5, duration=5.0, endnote='f#')
 
-# generate samples, note conversion to float32 array
-samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+# Add a 1-second tone on track 1, slide pitch from f# to g#)
+mixer.add_note(1, note='f#', octave=5, duration=1.0, endnote='g#')
 
-# for paFloat32 sample values must be in range [-1.0, 1.0]
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=fs,
-                output=True)
+# Mix all tracks into a single list of samples and write to .wav file
+mixer.write_wav('tones.wav')
 
-# play. May repeat with different volume values (if done interactively)
-stream.write(volume*samples)
+# Mix all tracks into a single list of samples scaled from 0.0 to 1.0, and
+# return the sample list
+samples = mixer.mix()
 
-stream.stop_stream()
-stream.close()
-
-p.terminate()
