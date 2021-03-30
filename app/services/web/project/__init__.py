98d01a9a45794f1f40c8a650d7a199cb428a066d
Spacener <spencer.stepanek@colorado.edu>
2021-03-30 01:22:28 -0600

    rm dead css file, upload func init commit

--- app/services/web/project/__init__.py
+++ app/services/web/project/__init__.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+import os
+from flask_dropzone import Dropzone
 import photophonic as pp # main audio generation and image processing definitions
 
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
-
+dropzone = Dropzone(app)
 
 class User(db.Model):
     __tablename__ = "users"
@@ -18,10 +21,43 @@ class User(db.Model):
     def __init__(self, email):
         self.email = email
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+UPLOAD_FOLDER = '/project/static/uuids/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+app.config['SECRET_KEY'] = 'something only you know'
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/upload/", methods=['POST'], endpoint='upload_file')
+def upload():
+    if request.method == 'POST':
+        if request.form['button'] == 'upload':
+            print("[INFO]: Request got!")
+
+            # Checks if file is empty (page was loaded without one)
+            if 'file' not in request.files:
+                flash('No file part')
+                return render_template("home.html")
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit a empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return render_template("home.html")
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                print('file uploaded successfully')
+        else:
+            print("[INFO]: DEAD BUTTON!")
+            return render_template("home.html")
 
 
 @app.route("/login")
