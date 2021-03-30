ed4a5076e964c148aeb125d733df0c1309142201
Luke0328 <curly0328@gmail.com>
2021-03-30 13:17:52 -0600

    Login page flask

--- app/services/web/project/__init__.py
+++ app/services/web/project/__init__.py
@@ -62,9 +62,15 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect("/")
+    return render_template("login.html", error=error)
 
 
 @app.route("/account_reg")
